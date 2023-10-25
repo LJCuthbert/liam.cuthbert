2e2c2497dbccc5c552b93baedb53d041e9b92fee
LJCuthbert <liamjcuthbert@gmail.com>
2023-10-26 12:58:08 +1300

    Created a mouse follower and added gradients to the background

---
+++ src/pages/sections/Contact.tsx
@@ -0,0 +1,15 @@
+import TypewriterEffect from "../../components/Util/TypeWritterEffect.tsx";
+import {useInView} from "react-intersection-observer";
+
+const Contact = () => {
+    const [ref, inView] = useInView({ triggerOnce: false });
+
+    return (
+        <div className="min-h-screen relative transition-colors bg-gradient-to-b from-blue-300 dark:from-blue-800 flex justify-center items-center bg-green-300 dark:bg-green-800">
+            <div ref={ref} className="text-center">
+                {inView && <TypewriterEffect text="Contact Me" className={""}/>}
+            </div>
+        </div>
+    )
+}
+export default Contact;
