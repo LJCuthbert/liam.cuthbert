9c51fef6d422eeefd4550461a9718ac84838b345
Liam Cuthbert <lcu22@uclive.ac.nz>
2024-02-26 16:15:54 +1300

    Fixed mobile view for the details section and Header

--- src/pages/sections/Details.tsx
+++ src/pages/sections/Details.tsx
@@ -10,15 +10,15 @@ const Details = () => {
             <main className="p-4">
                 <div className="details flex flex-col gap-5 text-center">
 
-                    <h1 className="uppercase font-extrabold dark:text-gray-300 text-7xl flex gap-5">Liam
+                    <h1 className="uppercase font-extrabold dark:text-gray-300 text-7xl flex flex-col md:flex-row gap-5">Liam
                         <div ref={ref} className="text-center">
                             {inView && <TypewriterEffect text="Cuthbert"
                                                          className={"font-normal dark:text-rose-700 text-rose-300"}/>}
                         </div>
                     </h1>
-                    <span className="text-left">
+                    <span className="">
                         <span
-                            className="uppercase text-5xl flex flex-col md:flex-row gap-0 md:gap-3 text-amber-300 dark:text-amber-600">Creative <CreativeWords
+                            className="text-center md:text-left uppercase text-5xl flex flex-col md:flex-row gap-0 md:gap-3 text-amber-300 dark:text-amber-600">Creative <CreativeWords
                             words={['developer', 'programmer', 'designer', "person"]}/> </span>
 
                     </span>
