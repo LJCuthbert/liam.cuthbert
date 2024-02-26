bc5d92f2e3c39a04209977fd6888d89ccfe5865f
Liam Cuthbert <lcu22@uclive.ac.nz>
2024-02-27 12:21:02 +1300

    Added flowbite to use a carousel for the about me section to look better for mobile viewers

--- src/components/Util/Card.tsx
+++ src/components/Util/Card.tsx
@@ -3,11 +3,16 @@ import React, { ReactNode } from "react";
 interface CardProps {
     title: string;
     children: ReactNode;
+    className?: string;
 }
 
-const Card: React.FC<CardProps> = ({ title, children }) => (
-    <div className="bg-white p-6 shadow-md dark:bg-black dark:shadow-amber-100 sm:rounded-md">
-        <h2 className="mb-4 text-2xl font-bold">{title}</h2>
+const Card: React.FC<CardProps> = ({ title, children, className }) => (
+    <div
+        className={`${className} bg-white p-14 shadow-md  dark:bg-blue-900 dark:shadow-amber-100 sm:rounded-md sm:p-6`}
+    >
+        <h2 className="mb-4 text-center text-2xl font-bold sm:text-left">
+            {title}
+        </h2>
         {children}
     </div>
 );
--- src/pages/sections/AboutMe.tsx
+++ src/pages/sections/AboutMe.tsx
@@ -2,12 +2,19 @@ import Card from "../../components/Util/Card.tsx";
 import TypewriterEffect from "../../components/Util/TypeWritterEffect.tsx";
 import { useInView } from "react-intersection-observer";
 import CreativeWords from "../../components/Main/CreativeWords.tsx";
+import { useState } from "react";
+import { Carousel } from "flowbite-react";
 
 const AboutMe = () => {
     const [ref, inView] = useInView({ triggerOnce: false });
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    window.addEventListener("resize", () => {
+        setWindowWidth(window.innerWidth);
+    });
+
     return (
-        <div className="flex min-h-svh snap-start items-center justify-end gap-12 bg-blue-300 bg-gradient-to-b from-amber-300 from-10% py-6 transition-colors dark:bg-blue-800 dark:from-amber-800 md:min-h-screen md:justify-center">
-            <div className="md:container">
+        <div className="flex min-h-svh snap-start items-center justify-center gap-12 bg-blue-300 bg-gradient-to-b from-amber-300 from-10% py-6 transition-colors dark:bg-blue-800 dark:from-amber-800 md:min-h-screen">
+            <div className="container">
                 <div ref={ref} className="text-center">
                     {inView && (
                         <TypewriterEffect
@@ -17,56 +24,121 @@ const AboutMe = () => {
                     )}
                 </div>
                 <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
-                    {/* Personal Information Card */}
-                    <Card title="Personal Information">
-                        <div className="inline-block">
-                            Hi! I'm Liam , a passionate{" "}
-                            <CreativeWords
-                                words={[
-                                    "Computer Scientist",
-                                    "Frontend Developer",
-                                ]}
-                                className={["h-[1.5em]"]}
-                                color={[
-                                    "word-cycle-15em text",
-                                    "dark:text-blue-700",
-                                    "text-blue-400",
-                                ]}
-                            ></CreativeWords>{" "}
-                            based in Christchurch. I love coding, solving
-                            problems, and building things that make a
-                            difference.
-                        </div>
-                        {/* Add more personal information as needed */}
-                    </Card>
+                    {windowWidth <= 640 ? (
+                        <Carousel
+                            slideInterval={5000}
+                            slide={false}
+                            className="min-h-[6em] w-full"
+                        >
+                            {/* Personal Information Card */}
+                            <Card
+                                title="Personal Information"
+                                className={"h-full w-full"}
+                            >
+                                <div className="md:inline-block">
+                                    Hi! I'm Liam , a passionate{" "}
+                                    <CreativeWords
+                                        words={[
+                                            "Computer Scientist",
+                                            "Frontend Developer",
+                                        ]}
+                                        className={["h-[1.5em]"]}
+                                        color={[
+                                            "word-cycle-15em text",
+                                            "dark:text-blue-700",
+                                            "text-blue-400",
+                                        ]}
+                                    ></CreativeWords>{" "}
+                                    based in Christchurch. I love coding,
+                                    solving problems, and building things that
+                                    make a difference.
+                                </div>
+                                {/* Add more personal information as needed */}
+                            </Card>
+                            {/* Skills Card */}
+                            <Card title="Skills" className={"h-full w-full"}>
+                                <ul className="list-disc pl-4">
+                                    <li>
+                                        JavaScript (Vanilla, React, Node.js,
+                                        Typescript)
+                                    </li>
+                                    <li>HTML, CSS (Bootstrap, Tailwind)</li>
+                                    <li>Java, Thymeleaf, Springboot</li>
+                                    <li>Database (MySQL)</li>
+                                </ul>
+                            </Card>
+                            {/* Education Card */}
+                            <Card title="Education" className={"h-full w-full"}>
+                                <p>
+                                    Soon to have Bachelor's Degree in Computer
+                                    Science, University of Canterbury, 2024
+                                </p>
+                            </Card>
+                            {/* Experience Card */}
+                            <Card
+                                title="Experience"
+                                className={"h-full w-full"}
+                            >
+                                <p>
+                                    Currently working at Velocity Karts as a
+                                    Senior Track Instructor.
+                                </p>
+                            </Card>
+                        </Carousel>
+                    ) : (
+                        <>
+                            {/* Personal Information Card */}
+                            <Card title="Personal Information">
+                                <div className="inline-block">
+                                    Hi! I'm Liam , a passionate{" "}
+                                    <CreativeWords
+                                        words={[
+                                            "Computer Scientist",
+                                            "Frontend Developer",
+                                        ]}
+                                        className={["h-[1.5em]"]}
+                                        color={[
+                                            "word-cycle-15em text",
+                                            "dark:text-blue-700",
+                                            "text-blue-400",
+                                        ]}
+                                    ></CreativeWords>{" "}
+                                    based in Christchurch. I love coding,
+                                    solving problems, and building things that
+                                    make a difference.
+                                </div>
+                                {/* Add more personal information as needed */}
+                            </Card>
+                            {/* Skills Card */}
+                            <Card title="Skills">
+                                <ul className="list-disc pl-4">
+                                    <li>
+                                        JavaScript (Vanilla, React, Node.js,
+                                        Typescript)
+                                    </li>
+                                    <li>HTML, CSS (Bootstrap, Tailwind)</li>
+                                    <li>Java, Thymeleaf, Springboot</li>
+                                    <li>Database (MySQL)</li>
+                                </ul>
+                            </Card>
 
-                    {/* Skills Card */}
-                    <Card title="Skills">
-                        <ul className="list-disc pl-4">
-                            <li>
-                                JavaScript (Vanilla, React, Node.js, Typescript)
-                            </li>
-                            <li>HTML, CSS (Bootstrap, Tailwind)</li>
-                            <li>Java, Thymeleaf, Springboot</li>
-                            <li>Database (MySQL)</li>
-                        </ul>
-                    </Card>
+                            {/* Education Card */}
+                            <Card title="Education">
+                                <p>
+                                    Soon to have Bachelor's Degree in Computer
+                                    Science, University of Canterbury, 2024
+                                </p>
+                            </Card>
 
-                    {/* Education Card */}
-                    <Card title="Education">
-                        <p>
-                            Soon to have Bachelor's Degree in Computer Science,
-                            University of Canterbury, 2024
-                        </p>
-                    </Card>
-
-                    {/* Experience Card */}
-                    <Card title="Experience">
-                        <p>
-                            Currently working at Velocity Karts as a Senior
-                            Track Instructor.
-                        </p>
-                    </Card>
+                            {/* Experience Card */}
+                            <Card title="Experience">
+                                <p>
+                                    Currently working at Velocity Karts as a
+                                    Senior Track Instructor.
+                                </p>
+                            </Card>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
