7a3139c9d39a3e7162fedcf6cd3663fa82c1337f
Liam Cuthbert <lcu22@uclive.ac.nz>
2024-06-12 12:39:36 +1200

    added hover effect to DarkModeToggle.tsx. Also changing CustomCursor.tsx

--- src/components/effects/CustomCursor.tsx
+++ src/components/effects/CustomCursor.tsx
@@ -1,57 +1,117 @@
+import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
 import "./CustomCursor.css";
-import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
+
+const SVGNS = "http://www.w3.org/2000/svg";
+const config = { lines: 25 };
 
 const CustomCursor = () => {
-    const defaultSize = 15;
-    const enlargedSize = 30; // Size when hovering over text
-    const [position, setPosition] = useState({ x: 0, y: 0 });
-    const [currSize, setCurrSize] = useState(defaultSize);
+  const defaultSize = 15;
+  const enlargedSize = 30; // Size when hovering over text
+  const [position, setPosition] = useState({
+    x: window.innerWidth * 0.5,
+    y: window.innerHeight * 0.5,
+  });
+  const [currSize, setCurrSize] = useState(defaultSize);
 
-    useEffect(() => {
-        const handleMouseMove = (e: MouseEvent) => {
-            setPosition({ x: e.clientX, y: e.clientY });
-        };
+  const svgRef = useRef<SVGSVGElement | null>(null);
+  const circleRef = useRef<SVGCircleElement | null>(null);
+  const _ease = 0.8;
+  const lines = useRef<SVGLineElement[]>([]);
 
-        const handlePointerEnter = () => {
-            setCurrSize(enlargedSize);
-        };
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
 
-        const handlePointerLeave = () => {
-            setCurrSize(defaultSize);
-        };
+    const handlePointerEnter = () => {
+      setCurrSize(enlargedSize);
+    };
 
-        window.addEventListener("pointermove", handleMouseMove);
+    const handlePointerLeave = () => {
+      setCurrSize(defaultSize);
+    };
 
-        // Select all text elements
-        const textElements = document.querySelectorAll(
-            "p, span, a, h1, h2, h3, h4, h5, h6, li",
-        );
-        textElements.forEach((element) => {
-            element.addEventListener("pointerenter", handlePointerEnter);
-            element.addEventListener("pointerleave", handlePointerLeave);
-        });
+    window.addEventListener("pointermove", handleMouseMove);
 
-        return () => {
-            window.removeEventListener("pointermove", handleMouseMove);
-            textElements.forEach((element) => {
-                element.removeEventListener("pointerenter", handlePointerEnter);
-                element.removeEventListener("pointerleave", handlePointerLeave);
-            });
-        };
-    }, []);
-
-    return (
-        <motion.div
-            className="cursor"
-            style={{
-                translateX: position.x - currSize / 2,
-                translateY: position.y - currSize / 2,
-                height: `${currSize}px`,
-                width: `${currSize}px`,
-            }}
-        ></motion.div>
+    // Select all text elements
+    const textElements = document.querySelectorAll(
+      "p, span, a, h1, h2, h3, h4, h5, h6, li",
     );
+    textElements.forEach((element) => {
+      element.addEventListener("pointerenter", handlePointerEnter);
+      element.addEventListener("pointerleave", handlePointerLeave);
+    });
+
+    return () => {
+      window.removeEventListener("pointermove", handleMouseMove);
+      textElements.forEach((element) => {
+        element.removeEventListener("pointerenter", handlePointerEnter);
+        element.removeEventListener("pointerleave", handlePointerLeave);
+      });
+    };
+  }, []);
+
+  useEffect(() => {
+    if (svgRef.current) {
+      const linesArray: SVGLineElement[] = [];
+
+      for (let i = 0; i < config.lines; i++) {
+        const line = document.createElementNS(SVGNS, "line");
+        line.setAttribute("stroke", "black");
+        svgRef.current.appendChild(line);
+        linesArray.push(line);
+      }
+
+      lines.current = linesArray;
+      animateLines();
+    }
+  }, [svgRef]);
+
+  useEffect(() => {
+    if (circleRef.current) {
+      circleRef.current.setAttribute("cx", String(position.x));
+      circleRef.current.setAttribute("cy", String(position.y));
+    }
+    animateLines();
+  }, [position]);
+
+  const animateLines = () => {
+    console.log("Animating lines");
+    lines.current.forEach((line) => {
+      gsap.set(line, { x: position.x, y: position.y });
+
+      gsap.to(line, {
+        duration: 1,
+        x: "+=1",
+        y: "+=1",
+        repeat: -1,
+        ease: "none",
+        modifiers: {
+          x: () => {
+            const posX = parseFloat(gsap.getProperty(line, "x").toString());
+            const leaderX = position.x;
+            const x = posX + (leaderX - posX) * _ease;
+            line.setAttribute("x2", String(leaderX - x));
+            return String(x);
+          },
+          y: () => {
+            const posY = parseFloat(gsap.getProperty(line, "y").toString());
+            const leaderY = position.y;
+            const y = posY + (leaderY - posY) * _ease;
+            line.setAttribute("y2", String(leaderY - y));
+            return String(y);
+          },
+        },
+      });
+    });
+  };
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" ref={svgRef} className="cursor-svg">
+      <circle fill="black" r={currSize} ref={circleRef} />
+    </svg>
+  );
 };
 
 export default CustomCursor;
--- src/components/theme/DarkModeToggle.tsx
+++ src/components/theme/DarkModeToggle.tsx
@@ -1,26 +1,47 @@
 import { useTheme } from "./ThemeContext.tsx";
+import { useEffect, useState } from "react";
 
 const DarkModeToggle = () => {
-    const { theme, toggleTheme } = useTheme();
-    return (
-        <div className="my-auto">
-            <label htmlFor="toggle" className="flex items-center">
-                <div className="relative">
-                    <input
-                        id="toggle"
-                        type="checkbox"
-                        className="hidden"
-                        onChange={toggleTheme}
-                        checked={theme === "dark"}
-                    />
-                    <div className="toggle__line h-6 w-12 rounded-full bg-blue-400 shadow-inner transition-colors duration-500 dark:bg-amber-500"></div>
-                    <div
-                        className={`toggle__dot absolute inset-y-0 left-0 h-6 w-6 transform rounded-full bg-white shadow transition-transform duration-300 ease-in-out ${theme === "dark" ? "translate-x-full" : "translate-x-0"}`}
-                    ></div>
-                </div>
-            </label>
+  const { theme, toggleTheme } = useTheme();
+  const [hover, setHover] = useState(false);
+  const handleMouseEnter = () => {
+    setHover((prevState) => !prevState);
+  };
+  const handleMouseLeave = () => {
+    setHover((prevState) => !prevState);
+  };
+  useEffect(() => {
+    const element = document.getElementById("buttonToggle");
+    if (!element) return;
+
+    element.addEventListener("mouseenter", handleMouseEnter);
+    element.addEventListener("mouseleave", handleMouseLeave);
+    return () => {
+      element.removeEventListener("mouseenter", handleMouseEnter);
+      element.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
+  return (
+    <div className="my-auto" id="buttonToggle">
+      <label htmlFor="toggle" className="flex items-center">
+        <div className="relative">
+          <input
+            id="toggle"
+            type="checkbox"
+            className="hidden"
+            onChange={toggleTheme}
+            checked={theme === "dark"}
+          />
+          <div
+            className={`toggle__line h-6 w-12 rounded-full shadow-inner transition ${hover ? "bg-blue-400/70 dark:bg-amber-500/70" : "bg-blue-400 dark:bg-amber-500"}`}
+          ></div>
+          <div
+            className={`toggle__dot absolute inset-y-0 left-0 h-6 w-6 transform rounded-full ${hover ? "bg-slate-100" : "bg-white"} shadow transition ease-in-out ${theme === "dark" ? "translate-x-full" : "translate-x-0"}`}
+          ></div>
         </div>
-    );
+      </label>
+    </div>
+  );
 };
 
 export default DarkModeToggle;
